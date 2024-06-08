887b90551bf2badc998bb7eb4d9751b52b950cdc
kanyesteves <kany.esteves@gmail.com>
2024-06-08 13:11:13 -0300

    Iniciado projeto e criando ambiente para desenvolvimento

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    layout="wide",
+    page_title="Esboço"
+)
+
+st.title("Pedido")
+
+col1, col2 = st.columns(2)
+lanche = col1.selectbox("Lanche", ["Cachorro Quente", "Porção"])
+if "Cachorro Quente" in lanche:
+    col2.selectbox("Adicional", ["Milho verde", "Cheedar", "Catupiry", "Mussarela", "Bacon", "Calabresa"])
+else: 
+    col2.selectbox("Adicional", ["Bacon", "Calabresa"])
